7ef129bc2a726c12b85da8085e8287a23df3600b
Adedamola Shittu <shittuadedamola@rocketmail.com>
2022-01-03 17:24:21 +0100

    Add New And Create Actions For Category Form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,36 @@
 class CategoriesController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @categories = current_user.categories.order(:created_at)
+  end
+
+  def show
+    @category = Category.includes(:exchanges).find(params[:id])
+    @exchanges = @category.recent_exchanges
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.build(category_params(:name, :icon))
+
+    respond_to do |format|
+      format.html do
+        if @category.save
+          redirect_to authenticated_root_path, notice: 'Category has been successfully created'
+        else
+          render new_category_path, alert: 'Category not created. Please, try again!'
+        end
+      end
+    end
+  end
+
+  private
+
+  def category_params(*args)
+    params.require(:category).permit(*args)
+  end
 end

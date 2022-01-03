5eada83825ca220ba3b5e232fd69eac4cd0ef28a
Adedamola Shittu <shittuadedamola@rocketmail.com>
2022-01-03 17:25:12 +0100

    Add Validations To Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+
   has_many :categories, dependent: :destroy
   has_many :exchanges, dependent: :destroy, foreign_key: 'author_id'
 end

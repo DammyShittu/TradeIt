08ffae989c75c021112883794d314945a317c1ea
Adedamola Shittu <shittuadedamola@rocketmail.com>
2022-01-03 10:36:03 +0100

    Generate Join Table Migration And Migrate Database

---
+++ db/migrate/20220103093321_create_categories_and_exchanges.rb
@@ -0,0 +1,24 @@
+class CreateCategoriesAndExchanges < ActiveRecord::Migration[6.1]
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.string :icon
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    create_table :exchanges do |t|
+      t.string :name
+      t.integer :amount
+      t.references :author, null: false, foreign_key: {to_table: :users}
+
+      t.timestamps
+    end
+
+    create_table :categories_exchanges, id: false do |t|
+      t.belongs_to :category
+      t.belongs_to :exchange
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_01_03_093321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_exchanges", id: false, force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "exchange_id"
+    t.index ["category_id"], name: "index_categories_exchanges_on_category_id"
+    t.index ["exchange_id"], name: "index_categories_exchanges_on_exchange_id"
+  end
+
+  create_table "exchanges", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_exchanges_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "categories", "users"
+  add_foreign_key "exchanges", "users", column: "author_id"
 end
